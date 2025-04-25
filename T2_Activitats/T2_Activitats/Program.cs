575db2e9541f1e1ec6f14f457fbe08fa30ea914e
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-04-25 23:12:20 +0200

    Exercici 2 fet!

--- T2_Activitats/T2_Activitats/Program.cs
+++ T2_Activitats/T2_Activitats/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Diagnostics;
+using System.IO;
+namespace T2_Activitats
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //Exercici 2
+            //Realitza un programa que imprimeixi per pantalla tots el nom i el PID (Process ID) que estan en execució de la màquina que estàs fent servir. Guarda aquesta llista en un arxiu de text.
+
+            var Processos = Process.GetProcesses();
+            string fitxerProcessos = @"..\..\..\processos.txt";
+            using (StreamWriter writter = new StreamWriter(fitxerProcessos))
+            {
+                foreach (var Process in Processos)
+                {
+                    writter.WriteLine(Process.ProcessName + " " + Process.Id);
+                    Console.WriteLine(Process.ProcessName + " "+ Process.Id);
+
+                }
+            }
+        }
+    }
+}
